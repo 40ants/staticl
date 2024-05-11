b9e9ae6fea4e1bc257a2ec6affe7bfbd64b4141b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-05-11 12:54:46 +0300

    Updated a changelog.

--- docs/changelog.lisp
+++ docs/changelog.lisp
@@ -9,5 +9,8 @@
                               "ASDF"
                               "REPL"
                               "HTTP"))
-  (0.1.0 2023-02-05
+  (0.1.1 2024-05-11
+         "* Fixed error in `serve` command when xdg-open ultility is not available.
+          * Installation using Roswell was updated in the docs.")
+  (0.1.0 2024-05-10
          "* Initial version."))
