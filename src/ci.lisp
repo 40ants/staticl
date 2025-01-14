6b2b7c088c59f09f1d476be4272d1af29528c011
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-01-14 15:28:53 +0000

    Check imports on CI.

--- src/ci.lisp
+++ src/ci.lisp
@@ -18,7 +18,8 @@
   :jobs ((40ants-ci/jobs/linter:linter
           :asdf-systems ("staticl"
                          "staticl-docs"
-                         "staticl-tests"))))
+                         "staticl-tests")
+          :check-imports t)))
 
 (defworkflow docs
   :on-push-to "master"
